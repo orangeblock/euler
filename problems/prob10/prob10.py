d3a226689005784a90d347eb1488a8f8b97d0006
Hardc0de <dan.datsev@gmail.com>
2013-04-21 12:59:14 +0300

    10 fix

--- problems/prob10/prob10.py
+++ problems/prob10/prob10.py
@@ -3,8 +3,8 @@ def sieve(limit):
     l = [2] + range(3, limit+1, 2) # use 2 and only odd numbers
     for i, num in enumerate(l):
         if num and i > 0:
-            # remove all multiples of i which for odd numbers are:
-            # 2i(i+1), 2i(i+1) + 2i+1, 2i(i+1) + 4i+2...
+            # remove all multiples of l[i] which for odd numbers
+            # are located at indexes: 2i(i+1), 2i(i+1) + 2i+1, 2i(i+1) + 4i+2...
             for j in range(2*i*(i+1), len(l), 2*i+1):
                 l[j] = False
     return [x for x in l if x]
